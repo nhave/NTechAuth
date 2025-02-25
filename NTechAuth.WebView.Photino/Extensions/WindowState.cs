6f620552205e511afcbc5f9bc1105f7ddae3b879
Nikolaj Have <nhave06@gmail.com>
2025-02-26 00:19:09 +0100

    Created a PhotinoBlazorApp

---
+++ NTechAuth.WebView.Photino/Extensions/WindowState.cs
@@ -0,0 +1,60 @@
+﻿using Photino.Blazor;
+using System.IO;
+using System.Text.Json;
+using Photino.NET;
+using System;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class WindowUtils
+    {
+        public static void AddWindowStateHandler(this PhotinoBlazorApp app)
+        {
+            var windowState = LoadWindowState();
+            if (windowState != null)
+            {
+                app.MainWindow.SetSize(windowState.Width, windowState.Height);
+                app.MainWindow.SetMaximized(windowState.IsMaximized);
+            }
+
+            app.MainWindow.RegisterSizeChangedHandler((sender, e) =>
+            {
+                if (!app.MainWindow.Maximized)
+                {
+                    windowState.Width = app.MainWindow.Width;
+                    windowState.Height = app.MainWindow.Height;
+                }
+                windowState.IsMaximized = app.MainWindow.Maximized;
+            });
+
+            app.MainWindow.RegisterWindowClosingHandler((sender, e) =>
+            {
+                SaveWindowState(app.MainWindow, windowState);
+                return false;
+            });
+        }
+
+        private static void SaveWindowState(PhotinoWindow window, WindowState windowState)
+        {
+            var json = JsonSerializer.Serialize(windowState);
+            File.WriteAllText("windowState.json", json);
+        }
+
+        private static WindowState LoadWindowState()
+        {
+            if (File.Exists("windowState.json"))
+            {
+                var json = File.ReadAllText("windowState.json");
+                return JsonSerializer.Deserialize<WindowState>(json);
+            }
+            return null;
+        }
+    }
+
+    public class WindowState
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public bool IsMaximized { get; set; }
+    }
+}
---
+++ NTechAuth.WebView.Photino/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using NTechAuth.WebView.Components;
+using Photino.Blazor;
+
+namespace NTechAuth.WebView.Photino
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var appBuilder = PhotinoBlazorAppBuilder.CreateDefault(args);
+
+            appBuilder.Services
+                .AddLogging();
+
+            appBuilder.Services.AddSharedServices();
+
+            // register root component and selector
+            appBuilder.RootComponents.Add<Routes>("app");
+
+            var app = appBuilder.Build();
+
+            // customize window
+            app.MainWindow
+                .SetIconFile("favicon.ico")
+                .SetTitle("NTech Authentication");
+
+            app.AddWindowStateHandler();
+
+            AppDomain.CurrentDomain.UnhandledException += (sender, error) =>
+            {
+                app.MainWindow.ShowMessage("Fatal exception", error.ExceptionObject.ToString());
+            };
+
+            app.Run();
+
+        }
+    }
+}
